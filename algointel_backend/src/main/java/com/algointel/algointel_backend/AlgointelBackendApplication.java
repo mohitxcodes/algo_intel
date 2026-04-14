94498caef38431c895a01ed976c856c177ee7a4c
Mohit Singh <mohitsinghx12@gmail.com>
2026-04-14 21:17:15 +0530

    backend add

---
+++ algointel_backend/src/main/java/com/algointel/algointel_backend/AlgointelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.algointel.algointel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgointelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgointelBackendApplication.class, args);
+	}
+
+}
---
+++ algointel_backend/src/test/java/com/algointel/algointel_backend/AlgointelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algointel.algointel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgointelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
